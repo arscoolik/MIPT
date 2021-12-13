b55ae42011291abfcba32e052de52296fcb7d29a
Arseniy Sitnikov <74531830+arscoolik@users.noreply.github.com>
2021-12-13 20:59:31 +0300

    Create main.cpp

---
+++ OOP/Circle/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "Point.h"
+#include "Point.cpp"
+#include "Circle.h"
+#include "Circle.cpp"
+#include <math.h>
+using namespace std;
+
+
+
+
+int main()
+{
+	Point p = {7, -1};
+	Point q = {-4, 2};
+	cout << "Point p = " << p << endl;
+	cout << "Point q = " << q << endl;
+	cout << "p + q = " << p + q << endl;
+
+
+	Circle a {{4, 1}, 3};
+	Circle b;
+
+	cout << "Circle a: center: " << a.get_center() << " radius: " << a.get_radius() << endl;
+	cout << "Circle b: center: " << b.get_center() << " radius: " << b.get_radius() << endl;
+
+	cout << "Area of a = " << a.get_area() << endl;
+	cout << "Distance from point p to circle a = " << a.get_distance(p) << endl;
+
+
+
+	cout << "Collisions:" << endl;
+	if (a.is_colliding(b))
+		cout << "Yes, a is colliding b" << endl;
+	else
+		cout << "No, a isn't colliding b" << endl;
+
+
+
+	cout << "Moving b by {1, 1}:" << endl;
+	b.move({1, 1});
+	if (a.is_colliding(b))
+		cout << "Yes, a is colliding b" << endl;
+	else
+		cout << "No, a isn't colliding b" << endl;
+
+
+
+}
